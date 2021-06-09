3e2556533e06d4ade9c7d274a712eece059eedac
Natan Riegler <natan.riegler.silva@gmail.com>
2021-06-08 23:49:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/pluglight03/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.local.pluglight03;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+    }
+
+}
